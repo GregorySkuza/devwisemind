c2f094de1497f76838c8b2dd4eddd14226316a55
GregorySkuza <gskuza94@gmail.com>
2020-11-30 16:14:44 +0100

    Initial commit

---
+++ src/main/java/pl/gskuza/devwisemind/DevwisemindApplication.java
@@ -0,0 +1,13 @@
+package pl.gskuza.devwisemind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevwisemindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevwisemindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/gskuza/devwisemind/DevwisemindApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gskuza.devwisemind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevwisemindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
